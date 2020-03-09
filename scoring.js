e04d83ccb6cbc08623378c5017d49bac630503a0
月野木勝洋 <tsukinokikatsuhiro@gatsunogikatsuhironoMacBook-Air.local>
2020-03-09 18:17:50 +0900

    initial commit

--- scoring.js
+++ scoring.js
@@ -1,16 +1,21 @@
 // DOMの読み込みが終わったらfunction()の中の処理を実行します。
-$(document).ready(function(){
-  // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]の入力値を取得して合計点と平均点を出すロジックを作ります。
-  function score_indicate(){
-    // 変数「subject_points」に
-    // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]の配列を代入します。
+$(document).ready(function(){points
+  function points(){
     let subject_points = [Number($('#national_language').val()),
                           Number($('#english').val()),
                           Number($('#mathematics').val()),
                           Number($('#science').val()),
                           Number($('#society').val())
                           ];
+    return subject_points;
+  };
+  // [国語の点数,英語の点数数学の点数,理科の点数,社会の点数]の入力値を取得して合計点と平均点を出すロジックを作ります。
+  function score_indicate(){
+    // 変数「subject_points」に
+    // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]の配列を代入します。
+    // let subject_points = points
     // 変数「sum」に
+    let subject_points = points();
     // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]をそれぞれ足します。
     // ヒント! 配列を一つづつ取り出して足していきます。
     let sum = subject_points[0];
@@ -26,7 +31,7 @@ $(document).ready(function(){
     let average = sum / subject_points.length;
     $("#average_indicate").text(average);
     return average;
-  // };
+  };
 
   // 平均点数を取得し、取得した平均点数からランク分け("A", "B", "C", "D")をするロジックを作ります。
   function get_achievement(){
@@ -52,22 +57,23 @@ $(document).ready(function(){
   };
   // 平均値を取得し、取得した平均点からランク分け("A", "B", "C", "D")をするロジックを作ります。
 
-  function get_pass_or_failure(subject_points){
+  function get_pass_or_failure(){
+    let subject_points = points();
     // 変数「number」に入力した教科の数を代入します。
-    let number = subject_points
+    let number = subject_points.length;
     // 変数「judge」に"合格"を代入しておきます。
     let judge = "合格";
     // 入力したそれぞれの教科の点数が60点よりも低いと変数「judge」に"不合格"を代入して「judge」を返します。
     // ヒント! 「javascript 点数 合格 不合格 ロジック」で検索してみてください。
-    for(let i=0; i<points; i++){
-      if(points[i] < 60){
+    for(let i=0; i<number; i++){
+      if(subject_points[i] < 60){
         judge　=  "不合格";
         break;
       }
     }
     return judge;
-    }
-    $("#judge").text(get_pass_or_failure()); //subuject_points
+
+    // $("#judge").text(get_pass_or_failure()); //subuject_points
   };
   // 最終的なジャッジのロジックを作ります。
   function judgement(){
@@ -78,19 +84,25 @@ $(document).ready(function(){
     // 「最終ジャッジ」(id="alert-indicate)ボタンを押したら「あなたの成績は${achievement}で${pass_or_failure}です」が出力される処理です。
     $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${achievement}で${pass_or_failure}です</label>`);
     };
-  // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]のいずれかの点数が変更された際に「function score_indicate()」を発火させる処理です。
+
+    // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]のいずれかの点数が変更された際に「function score_indicate()」を発火させる処理です。
     $('#national_language, #english, #mathematics, #science, #society').change(function() {
       score_indicate();
     });
-  // 「ランク」(class="evaluation")ボタンを押したら「get_achievement()」が出力される処理です。
+
+    // 「ランク」(class="evaluation")ボタンを押したら「get_achievement()」が出力される処理です。
     $('#btn-evaluation').click(function() {
       $('#evaluation').text(get_achievement());
     });
-  // 「判定」(class="btn-judge")ボタンを押したら「function et_pass_or_failure()」が出力される処理です。
+
+    // 「判定」(class="btn-judge")ボタンを押したら「function et_pass_or_failure()」が出力される処理です。
     $('#btn-judge').click(function() {
+      $('#judge').text(get_pass_or_failure());
     });
-  // 「最終ジャッジ」(class="btn-declaration")ボタンを押したら「function judgement()」が出力される処理です。
+
+    // 「最終ジャッジ」(class="btn-declaration")ボタンを押したら「function judgement()」が出力される処理です。
     $('#btn-declaration').click(function() {
+      $('#declaration').text(judgement());
     });
   });
 // ここに書かれているjsの記述はあくまでヒントとして用意された雛形なので、書かれている記述に従わずに実装したいという場合は、自分の好きに実装して構わない。課題要件を満たし、コードの品質が一定の水準にあると判定されればどのような実装でも合格になる。
